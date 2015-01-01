2bae2b4e2414be2e3ddc45f461cf5d964c92b068
Sharunya <alexandr.lihacheff@gmail.com>
2015-01-01 06:15:58 +0500

    second commit

---
+++ src/main/java/ru/drvsh/MyServlet.java
@@ -0,0 +1,33 @@
+package ru.drvsh;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by lihachev on 01.01.15.
+ */
+//@WebServlet("/")
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        String varTextA = "Hello Worldd!";
+        request.setAttribute("textA", varTextA);
+        String varTextB = "It JSP.";
+        request.setAttribute("textB", varTextB);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
+        dispatcher.forward(request, response);
+    }
+}
--- src/main/java/ru/drvsh/Serv.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by lihachev on 07.12.14.
- */
-package ru.drvsh;
-
-import javax.servlet.http.HttpServlet;
-
-public class Serv extends HttpServlet {
-}
